9ee8d703703dba0eac8ad4bd4f5db9e2cb2e0407
neerojstha <nirojstha@hotmail.com>
2022-09-04 23:48:36 +0000

    loop to user data  until data is valid

--- run.py
+++ run.py
@@ -17,21 +17,25 @@ def get_sales_data():
     """
     Collecting sales details fromm the user
     """
-    print("please enter last day sales.")
-    print("data should be six numbers and separated by commas.")
-    print("Example: 11,22,33,44,55\n")
+    while True:
+        print("please enter last day sales.")
+        print("data should be five numbers and separated by commas.")
+        print("Example: 11,22,33,44,55\n")
 
-    data_str = input("submit your data here:")
+        data_str = input("submit your data here:")
     
-    sales_data  = data_str.split(",")
-    validate_data(sales_data)
+        sales_data  = data_str.split(",")
+        if validate_data(sales_data):
+            print("Data is valid")
+            break
+    return sales_data
 
 def validate_data(values):
     """
     inside try function check the validity of the data 
     with user submitted data
     """
-    print(values)
+  
     try:
         if len(values) != 5:
             raise ValueError(
@@ -39,7 +43,7 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"invalid data: {e}, please try again.\n")
-        print(values)
+        return False
 
 
-get_sales_data()
+data = get_sales_data()
